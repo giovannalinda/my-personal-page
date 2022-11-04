677bcb7dd021bc29f251d9d82ffcfa6386ec5aea
Giovanna Linda <eugiovannasouza@gmail.com>
2022-11-04 15:12:15 -0300

    feat: remove button component

--- src/components/Button/Button.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, Story } from '@storybook/react'
-import { Button } from './Button'
-
-export default {
-  title: 'Button',
-  component: Button,
-} as Meta
-
-export const Default: Story = () => <Button />
