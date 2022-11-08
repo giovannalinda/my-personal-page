c419e1f2c275d15d02bda9f6994532720c7a365b
Giovanna Linda <eugiovannasouza@gmail.com>
2022-11-08 12:36:22 -0300

    feat: translation menu component

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,18 +1,23 @@
 import Link from 'next/link'
+
+import { useTranslation } from '~/lib'
+
 import * as S from './Menu.styled'
 
 export function Menu() {
+  const { t } = useTranslation()
+
   return (
     <S.Container>
       <S.List>
         <S.ListItem>
-          <Link href='/'>INÍCIO</Link>
+          <Link href='/'>{t('HOME')}</Link>
         </S.ListItem>
         <S.ListItem>
-          <Link href='/about-me'>QUEM SOU</Link>
+          <Link href='/about-me'>{t('WHO I AM')}</Link>
         </S.ListItem>
         <S.ListItem>
-          <Link href='/contact'>CONTATO</Link>
+          <Link href='/contact'>{t('CONTACT')}</Link>
         </S.ListItem>
       </S.List>
     </S.Container>
