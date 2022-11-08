54f42c0b4d56a5794bd3111361ca1e4042b33c45
Giovanna Linda <eugiovannasouza@gmail.com>
2022-11-08 00:35:16 -0300

    feat: responsive testimonial section

--- src/components/Testimonial/Testimonial.tsx
+++ src/components/Testimonial/Testimonial.tsx
@@ -7,34 +7,37 @@ import * as S from './Testimonial.styled'
 
 export function Testimonial() {
   return (
-    <S.Container>
-      <GoQuote size={28} />
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={80}
-        slidesPerGroup={1}
-        loop={true}
-        pagination={{
-          type: 'fraction',
-        }}
-        modules={[Pagination, Navigation]}
-      >
-        <SwiperSlide>
-          <S.Title>
-            Trabalhar com a Giovanna foi demais! Ela é uma pessoa super
-            atenciosa e dedicada. Sempre se esforça ao máximo para melhorar o
-            seus resultados e agregar algo a mais ao time, fora a sua
-            organização, que é coisa de outro mundo.
-          </S.Title>
-        </SwiperSlide>
-        <SwiperSlide>
-          <S.Title>
-            A Giovanna é uma ótima profissional, sempre trazendo novas ideias ao
-            time, muito esforçada e inteligente.
-          </S.Title>
-        </SwiperSlide>
-      </Swiper>
-      <S.Paragraph>Recomendações do Linkedin</S.Paragraph>
-    </S.Container>
+    <>
+      <S.Title>O que dizem</S.Title>
+      <S.Container>
+        <GoQuote size={28} />
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={80}
+          slidesPerGroup={1}
+          loop={true}
+          pagination={{
+            type: 'fraction',
+          }}
+          modules={[Pagination, Navigation]}
+        >
+          <SwiperSlide>
+            <S.Recommendation>
+              Trabalhar com a Giovanna foi demais! Ela é uma pessoa super
+              atenciosa e dedicada. Sempre se esforça ao máximo para melhorar o
+              seus resultados e agregar algo a mais ao time, fora a sua
+              organização, que é coisa de outro mundo.
+            </S.Recommendation>
+          </SwiperSlide>
+          <SwiperSlide>
+            <S.Recommendation>
+              A Giovanna é uma ótima profissional, sempre trazendo novas ideias
+              ao time, muito esforçada e inteligente.
+            </S.Recommendation>
+          </SwiperSlide>
+        </Swiper>
+        <S.Paragraph>Recomendações do Linkedin</S.Paragraph>
+      </S.Container>
+    </>
   )
 }
